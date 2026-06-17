255b703ba27b11b6eb3eab3ba7cf687f6935f086
Darryl Morley <darrylmorley.uk@gmail.com>
2026-06-17 19:42:55 +0100

    WhatBattery 0.1.0

--- Casks/whatbattery.rb
+++ Casks/whatbattery.rb
@@ -1,6 +1,6 @@
 cask "whatbattery" do
   version "0.1.0"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 "9e63966566aebaf5729f2c08895b3681aa1437deec5e5035fcd19ab414448566"
 
   url "https://github.com/darrylmorley/whatbattery/releases/download/v#{version}/WhatBattery.zip",
       verified: "github.com/darrylmorley/whatbattery/"
